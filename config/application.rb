49292f1375f24267c58e2e70a2a126c80d36f05c
Shane <shane@cd2solutions.co.uk>
2016-03-29 13:40:37 +0100

    this

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,8 @@ module UnityWebsite
     config.x.layout_blocks['resource'] = {
       # main_content: :block_text,
     }
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
