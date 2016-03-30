a2dc208df1a148e5d5b6ced32911af47241d5e91
Henry Morgan <hexmodr@gmail.com>
2016-03-30 10:45:24 +0100

    published date

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,16 @@
 class Blog < ActiveRecord::Base
 
+  after_create :set_published_date
+
   mount_uploader :banner, ImageUploader
 
   default_scope { order('published_date DESC') }
 
   validates :name, presence: true
+
+  def set_published_date
+    self.published_date ||= Date.today
+    self.save!
+  end
 
 end
